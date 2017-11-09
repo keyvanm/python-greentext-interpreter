7ce03da57548509afa8e2b99889904497f42b03a
Keyvan Mosharraf <Keyvan_Mosharraf@thecdm.ca>
2017-11-09 03:47:28 -0800

    tasks.py

---
+++ tasks.py
@@ -0,0 +1,12 @@
+tasks = [
+    "question1": {
+        'text': """
+                What is the output of this program?
+                >>> a = 2
+                >>> b = a
+                >>> b = 3
+                >>> print(a)
+                """,
+        'answer': 2
+    }
+]
